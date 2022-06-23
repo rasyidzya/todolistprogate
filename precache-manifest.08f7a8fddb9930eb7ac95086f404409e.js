d129b4a6779d9932f6b32ebbaef7143a31d8f685
rasyidzya <zyajamaluddin@gmail.com>
2022-06-23 23:02:36 +0700

    Updates

---
+++ precache-manifest.08f7a8fddb9930eb7ac95086f404409e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fa1573cc301e2f58a6f",
+    "url": "/todolistprogate/static/css/main.9e2ec803.chunk.css"
+  },
+  {
+    "revision": "6fa1573cc301e2f58a6f",
+    "url": "/todolistprogate/static/js/main.c0144a40.chunk.js"
+  },
+  {
+    "revision": "19f07a043825f85af621",
+    "url": "/todolistprogate/static/js/runtime~main.a95e2023.js"
+  },
+  {
+    "revision": "39d4179eb90d2d5f1b2a",
+    "url": "/todolistprogate/static/js/2.3afcdef4.chunk.js"
+  },
+  {
+    "revision": "db126962928049df7e6248b46db9a8cf",
+    "url": "/todolistprogate/index.html"
+  }
+];
